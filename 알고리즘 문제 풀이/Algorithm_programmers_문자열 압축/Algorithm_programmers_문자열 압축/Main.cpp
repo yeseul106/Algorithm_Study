1c4323ba2355fddb5e1dd57ed332b3e835c7aa8a
yeseul106 <cho898989@naver.com>
2022-02-28 00:53:03 +0900

    2020 KAKAO BLIND RECRUITMENT_Level 2_문자열 압축

---
+++ 알고리즘 문제 풀이/Algorithm_programmers_문자열 압축/Algorithm_programmers_문자열 압축/Main.cpp
@@ -0,0 +1,54 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	string s;
+	getline(cin, s);
+
+    int answer = s.length();
+
+    for (int size = 1; size <= s.length() / 2; size++) {
+        string new_s;
+        string before = "\n";
+        int cnt = 1;
+
+        for (int idx = 0; idx < s.length(); idx += size) {
+            string s_tmp = s.substr(idx, size);
+            //cout << s_tmp << endl;
+
+            // 처음 턴이 아닐 때
+            
+            if (before.compare("\n") != 0) {
+                if (before.compare(s_tmp) == 0) {
+                    cnt++;
+                    //cout << "cnt: " << cnt << endl;
+                }
+                else {
+                    if(cnt != 1)
+                        new_s += to_string(cnt);
+                    new_s += before;
+                    cnt = 1;
+                }
+            }
+
+            if (idx + size >= s.length()) {
+                if (cnt != 1)
+                    new_s += to_string(cnt);
+                new_s += s_tmp;
+            }
+
+            before = s_tmp;
+        }
+
+        //cout << new_s << endl;
+
+        if (answer > new_s.length()) {
+            answer = new_s.length();
+        }
+    }
+
+    cout << answer << endl;
+}
